ac08eed5f3dc499c8153c0f289a8b03db75de363
papamitra <papamitra@gmail.com>
2011-12-18 09:00:28 +0900

    EvalML1実装

--- src/main/scala/GenBytecode.scala
+++ src/main/scala/GenBytecode.scala
@@ -4,64 +4,70 @@
 import localhost.ml._
 import ch.epfl.lamp.fjbg._
 
-object Main extends App {
+class Code(cname:String) {
   val context = new FJBGContext()
   import JAccessFlags._
   val jclass = context.JClass(ACC_PUBLIC,
-   "Hello",
+    cname,
     "java/lang/Object",
     JClass.NO_INTERFACES,
     "")
 
-  // コンストラクタ生成
-  val initMethod = jclass.addNewMethod(ACC_PUBLIC,
-    "<init>",
-    JType.VOID,
-    JType.EMPTY_ARRAY,
-    new Array[String](0))
-
-  val init = initMethod.getCode().asInstanceOf[JExtendedCode]
-  init.emitALOAD_0
-  init.emitINVOKESPECIAL("java/lang/Object",
-    "<init>",
-    JMethodType.ARGLESS_VOID_FUNCTION)
-  init.emitRETURN()
-
-  // main生成
   val mainMethod =
     jclass.addNewMethod(ACC_PUBLIC | ACC_STATIC,
-      "main",
-      JType.VOID,
+      "run",
+      JType.INT,
       Array(new JArrayType(new JObjectType("java.lang.String"))),
       Array("args"))
 
   val maincode = mainMethod.getCode().asInstanceOf[JExtendedCode]
 
-  maincode.emitGETSTATIC("java.lang.System", "out", new JObjectType("java.io.PrintStream"))
-  maincode.emitLDC("Hello,World")
-  maincode.emitINVOKEVIRTUAL("java.io.PrintStream",
-    "println",
-    new JMethodType(JType.VOID, Array(new JObjectType("java.lang.String"))))
-  maincode.emitRETURN()
+}
 
-  import scala.tools.nsc.interpreter._  
+object parser extends MLParser {
+  def parse(args: String) = {
+    parseAll(expr, args)
+  }
+}
+
+object Main extends App {
+
+  import scala.tools.nsc.interpreter._
   import scala.tools.nsc.io.{ AbstractFile, VirtualDirectory }
 
   val vd = new VirtualDirectory("(memory)", None)
   val parent = Thread.currentThread.getContextClassLoader
+  //  val parent = null
   val afcl = new AbstractFileClassLoader(vd, parent)
-  Thread.currentThread.setContextClassLoader(afcl)
+  //  Thread.currentThread.setContextClassLoader(afcl)
 
-  val file = vd.fileNamed("Hello.class")
+  var i = 0
+  while (true) {
+    val line = Console.readLine()
+    if (line == ":quit") sys.exit
 
-  val outstream = new java.io.DataOutputStream(file.bufferedOutput)
-  jclass writeTo outstream
-  outstream.close()
+    try {
+      val ast = parser.parse(line).get
 
-//  val hello = Class.forName("Hello")
-  val hello = afcl.loadClass("Hello")
-  val thread = afcl.loadClass("java.lang.Thread")
-  val method = hello.getMethods.filter(_.getName == "main").head
-  method.invoke(null, null)
+      val className = "$line%d".format(i)
+      i += 1
+      val file = vd.fileNamed(className + ".class")
+      val outstream = new java.io.DataOutputStream(file.bufferedOutput)
 
+      val code = new Code(className)
+      import code._
+      ast.emit(maincode)
+      maincode.emitIRETURN()
+
+      jclass writeTo outstream
+      outstream.close()
+
+      val hello = afcl.loadClass(className)
+      val thread = afcl.loadClass("java.lang.Thread")
+      val method = hello.getMethods.filter(_.getName == "run").head
+      println(method.invoke(null, null))
+    } catch {
+      case e => println(e.getStackTraceString)
+    }
+  }
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,19 +2,76 @@
 package localhost.ml
 
 import scala.util.parsing.combinator._
+import ch.epfl.lamp.fjbg._
 
-abstract sealed class Expr
-case class IfExpr(val pred:Expr, texpr:Expr, fexpr:Expr) extends Expr
-case class BinOP(val lhs:Expr , val rhs:Expr) extends Expr
-case class IntExpr(val i:Int) extends Expr
-case class BoolExpr(val b:Boolean) extends Expr
+abstract sealed class Op{ def emit(code:JExtendedCode)}
+object Plus extends Op{  def emit(code:JExtendedCode){ code.emitIADD()}}
+object Minus extends Op{  def emit(code:JExtendedCode){ code.emitISUB()}}
+object Multi extends Op{  def emit(code:JExtendedCode){ code.emitIMUL()}}
+object Less extends Op{
+  def emit(code:JExtendedCode){ 
+    import code._
+    val succ = newLabel()
+    val ret = newLabel()
+    emitIF_ICMPLT(succ)
+    emitLDC(0)
+    emitGOTO(ret)
+    succ.anchorToNext()
+    emitLDC(1)
+    ret.anchorToNext()
+  }
+}
+
+abstract sealed class Expr{
+  def emit(code:JExtendedCode):Unit
+}
+
+case class IntLiteral(val value:Int) extends Expr{
+  def emit(code:JExtendedCode){
+    code.emitLDC(value)
+  }
+}
+case class StringLiteral(val value:String) extends Expr{
+  def emit(code:JExtendedCode){
+    throw new Exception("StringLiteral emit not Implemented")
+  }
+}
+case class IfExpr(val pred:Expr, val texpr:Expr, val fexpr:Expr) extends Expr{
+  def emit(code:JExtendedCode){
+    import code._
+    val fail = newLabel()
+    val ret = newLabel()
+    pred.emit(code)
+    emitIFEQ(fail)
+    texpr.emit(code)
+    emitGOTO(ret)
+    fail.anchorToNext()
+    fexpr.emit(code)
+    ret.anchorToNext()
+  }
+}
+case class BinOp(val op:Op, val lhs:Expr , val rhs:Expr) extends Expr{
+  def emit(code:JExtendedCode){
+    lhs.emit(code)
+    rhs.emit(code)
+    op.emit(code)
+  }
+}
+
+case class BoolLiteral(val b:Boolean) extends Expr{
+  def emit(code:JExtendedCode){
+    throw new Exception("Boolean emit not Implemented")
+  }
+}
 
 class MLParser extends JavaTokenParsers{
-  def int: Parser[Any] = decimalNumber | "-"~decimalNumber
-  def bool: Parser[Any] = "true" | "false"
-  def value: Parser[Any] = int | bool
-  def factor: Parser[Any] = value | "if"~expr~"then"~expr~"else"~expr
-  def term: Parser[Any] = factor~"*"~factor | factor
-  def exp: Parser[Any] = term~("+"~term | "-"~term)|term
-  def expr: Parser[Any] = exp~"<"~exp|exp
+  def int: Parser[Expr] = (decimalNumber ^^ { case n=> IntLiteral(n.toInt)}
+			   | "-"~decimalNumber ^^ { case "-"~n => IntLiteral(-n.toInt)} )
+
+  def bool: Parser[Expr] = "true" ^^ (_=>BoolLiteral(true)) | "false" ^^ (_=>BoolLiteral(false))
+  def value: Parser[Expr] = int | bool
+  def factor: Parser[Expr] = value | "if"~expr~"then"~expr~"else"~expr ^^ { case "if"~pred~"then"~texpr~"else"~fexpr => IfExpr(pred, texpr, fexpr)} | expr
+  def term: Parser[Expr] = factor~"*"~term ^^ { case lhs~"*"~rhs => BinOp(Multi, lhs, rhs)} | factor
+  def exp: Parser[Expr] = term~"+"~exp ^^ {case lhs~"+"~rhs => BinOp(Plus,lhs,rhs)} | term~"-"~exp ^^ {case lhs~"-"~rhs => BinOp(Minus,lhs,rhs)} |term
+  def expr: Parser[Expr] = exp~"<"~exp ^^ {case lhs~"<"~rhs => BinOp(Less,lhs,rhs)} | exp
 }
