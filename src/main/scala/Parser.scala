b77cf7ec6b3f55bfeeb4aa0db865862c0152782c
papamitra <papamitra@gmail.com>
2011-12-23 19:40:50 +0900

    カッコに対応

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -18,8 +18,8 @@ case class BoolLiteral(val b:Boolean) extends Expr
 class MLParser extends JavaTokenParsers{
   def int: Parser[Expr] = (decimalNumber ^^ { case n=> IntLiteral(n.toInt)} | "-"~decimalNumber ^^ { case "-"~n => IntLiteral(-n.toInt)} )
   def bool: Parser[Expr] = "true" ^^ (_=>BoolLiteral(true)) | "false" ^^ (_=>BoolLiteral(false))
-  def value: Parser[Expr] = int | bool
-  def factor: Parser[Expr] = value | "if"~expr~"then"~expr~"else"~expr ^^ { case "if"~pred~"then"~texpr~"else"~fexpr => IfExpr(pred, texpr, fexpr)} | expr
+//  def value: Parser[Expr] = int | bool
+  def factor: Parser[Expr] = int | bool | "if"~expr~"then"~expr~"else"~expr ^^ { case "if"~pred~"then"~texpr~"else"~fexpr => IfExpr(pred, texpr, fexpr)} | "("~>expr<~")"
   def term: Parser[Expr] = factor~"*"~term ^^ { case lhs~"*"~rhs => BinOp(Mul, lhs, rhs)} | factor
   def exp: Parser[Expr] = term~"+"~exp ^^ {case lhs~"+"~rhs => BinOp(Add,lhs,rhs)} | term~"-"~exp ^^ {case lhs~"-"~rhs => BinOp(Sub,lhs,rhs)} | term
   def expr: Parser[Expr] = exp~"<"~exp ^^ {case lhs~"<"~rhs => BinOp(Less,lhs,rhs)} | exp
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -17,6 +17,12 @@ class MLParserSuite extends FunSuite{
     assert(BinOp(Add, IntLiteral(1), BinOp(Mul, IntLiteral(2), IntLiteral(3))) == parse("1 + 2 * 3").get)
     assert(IfExpr(BinOp(Less, IntLiteral(3), IntLiteral(4)), BoolLiteral(true), IntLiteral(5)) == parse("if 3 < 4 then true else 5").get)
     assert(BinOp(Add, IntLiteral(5), IfExpr(BinOp(Less, IntLiteral(-3), IntLiteral(-12)), IntLiteral(4), BoolLiteral(false))) == parse("5 + if -3 < -12 then 4 else false").get)
+
+    assert(BinOp(Add, IntLiteral(1), IntLiteral(2)) == parse("(1) + (2)").get)
+    assert(BinOp(Mul, BinOp(Add, IntLiteral(1), IntLiteral(2)) , IntLiteral(3)) == parse("(1+2) * 3").get)
+    assert(BinOp(Add, IfExpr(BoolLiteral(true), IntLiteral(1), IntLiteral(2)), IntLiteral(3)) == parse("(if true then 1 else 2) + 3").get)
+//    assert(LetExpr(Symbo("a"), IntLiteral(1), BinOp(Plus, Symbol("a"), IntLiteral(1))) == parse("let a = 1 in a + 1"))
+//    assert(BinOp(Sub, IntLiteral
   }
 
   test("eval expr"){
